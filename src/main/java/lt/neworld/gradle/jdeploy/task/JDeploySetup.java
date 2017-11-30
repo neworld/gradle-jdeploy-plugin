93d7cd7b80739e1119e148fc0e1b2c27584aa151
Andrius Semionovas <aneworld@gmail.com>
2017-11-30 21:49:18 +0200

    Make jdeploySetup task incremental

--- src/main/java/lt/neworld/gradle/jdeploy/task/JDeploySetup.java
+++ src/main/java/lt/neworld/gradle/jdeploy/task/JDeploySetup.java
@@ -1,9 +1,12 @@
 package lt.neworld.gradle.jdeploy.task;
 
+import com.moowork.gradle.node.NodeExtension;
 import com.moowork.gradle.node.npm.NpmTask;
 import lt.neworld.gradle.jdeploy.JDeployExtensionKt;
 import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.OutputDirectory;
 
+import java.io.File;
 import java.util.Arrays;
 
 /**
@@ -13,14 +16,31 @@ import java.util.Arrays;
 public class JDeploySetup extends NpmTask {
     public final static String NAME = "jdeploySetup";
 
+    private String version;
+
+    public JDeploySetup() {
+        getProject().afterEvaluate((project) ->
+                setVersion(JDeployExtensionKt.getJdeployExtension(project).getOptions().getToolVersion())
+        );
+    }
+
     @Input
-    public String version() {
-        return JDeployExtensionKt.getJdeployExtension(getProject()).getOptions().getToolVersion();
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    @OutputDirectory
+    public File getJdeployOutput() {
+        return new File(NodeExtension.get(getProject()).getNodeModulesDir(), "jdeploy");
     }
 
     @Override
     public void exec() {
-        setArgs(Arrays.asList("install", "jdeploy@" + version()));
+        setArgs(Arrays.asList("install", "jdeploy@" + getVersion()));
         super.exec();
     }
 }
